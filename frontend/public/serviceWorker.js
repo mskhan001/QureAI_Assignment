1a8a464e9c0a9e7d7ebd9584df557aa02005ceef
mskhan001 <mosahilkhan9@gmail.com>
2022-03-19 20:49:43 +0530

    Caching all Files | Service Worker Code Refactored

--- frontend/public/serviceWorker.js
+++ frontend/public/serviceWorker.js
@@ -1,36 +1,13 @@
 const cacheName = "v1";
-const cacheAssets = [
-  "index.html",
-  "static/js/bundle.js",
-  "ws",
-  "manifest.json",
-  "/",
-];
 
 // SERVICE WORKER LIFECYCLE
 // REGISTER --> INSTALL --> ACTIVATE
 
 // call the install event
-self.addEventListener("install", (e) => {
-  console.log("Service Worker : Installed ", e);
-
-  //   telling the browser to wait until this PROMISE is finsihed and it get's rid of this serviceWorker
-  e.waitUntil(
-    //   using the caches storage API
-    caches
-      .open(cacheName)
-      .then((cache) => {
-        console.log("Service Worker : Caching Files ", cache);
-        cache.addAll(cacheAssets);
-      })
-      .then(() => self.skipWaiting())
-  );
-});
+self.addEventListener("install", (e) => {});
 
 // call the activate event
 self.addEventListener("activate", (e) => {
-  console.log("Service Worker : Activated", e);
-
   // remove unwanted cache
   e.waitUntil(
     caches.keys().then((cacheNames) => {
@@ -48,15 +25,13 @@ self.addEventListener("activate", (e) => {
 
 // call the fetch event
 self.addEventListener("fetch", (e) => {
-  console.log("Service Worker : Fetching ", e);
   e.respondWith(
-    fetch(e.request)
-    // .then((res) => {
-    //   const resClone = res.clone();
-    //   caches.open(cacheName).then((cache) => {
-    //     cache.put(e.request, resClone);
-    //   });
-    //   return res;
-    // })
+    fetch(e.request).then((res) => {
+      const resClone = res.clone();
+      caches.open(cacheName).then((cache) => {
+        cache.put(e.request, resClone);
+      });
+      return res;
+    })
   ).catch((e) => caches.match(e.request).then((res) => res));
 });
