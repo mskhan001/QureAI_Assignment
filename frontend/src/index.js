0ef7a6c01b1e281f4481c03aa108e5f31cd045b6
mskhan001 <mosahilkhan9@gmail.com>
2022-03-19 20:47:23 +0530

    Added Service Worker | Caching pages

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./app/index";
 import reportWebVitals from "./reportWebVitals";
+import serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,3 +16,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+serviceWorkerRegistration();
